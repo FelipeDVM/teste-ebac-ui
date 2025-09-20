45b8a7a1d33650823622a8dff3cdac008c665ceb
Seu Nome <seuemail@exemplo.com>
2025-09-20 17:50:17 -0300

    lista

---
+++ cypress/e2e/3-login-ebac/produto.cy.js
@@ -0,0 +1,22 @@
+///<reference types="cypress"/>
+
+describe('produtos', () => { 
+
+beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    
+});
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+        //cy.get('.block-inner >')
+        //.first()
+        //.eq(2)
+        .contains('Argus All-Weather Tank')
+        .click()
+        cy.get('#tab-title-description > a').should('exist')
+
+
+    });
+    
+});
